70d375b66ed7b3b107927f08ce94fd89703556e8
Wyman <wymanleejun@gmail.com>
2016-04-19 10:11:09 +0800

    Initial Commit

---
+++ webrequest-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  webrequest-test
+//
+//  Created by Admin on 19/4/16.
+//  Copyright © 2016 Dreamsmart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
